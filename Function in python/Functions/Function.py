cfddd918eb58b64a7872532c676cef2834facba6
Manoj <manojgharti691@gmail.com>
2026-02-06 16:22:32 +0545

    Function

---
+++ Function in python/Functions/Function.py
@@ -0,0 +1,15 @@
+def sum(a,b):
+    print(a + b)
+
+sum(5,10)
+
+def greet(name):
+    print("Good Morning!" + name)
+
+greet("Manoj")
+
+def avg(x,y):
+    return (x+y)/2
+
+avarage = avg(50,20)
+print("The average value is", + avarage)
