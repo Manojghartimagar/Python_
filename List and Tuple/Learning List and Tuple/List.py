3c388e5623d525e615a2efab3de2eb893a0cdc79
Manoj <manojgharti691@gmail.com>
2026-02-06 09:09:37 +0545

    list

---
+++ List and Tuple/Learning List and Tuple/List.py
@@ -0,0 +1,4 @@
+list = ["Manoj","Mango",47,45.65,True]  #mutable,collection of diffent data type
+list[4] = False
+print(list) #['Manoj', 'Mango', 47, 45.65, False]
+print(list[0:5:2]) # 0 to 4 index by skiping one value n-1 
