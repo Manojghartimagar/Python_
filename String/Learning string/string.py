9b18a2759ab52aeb38ec2499ec429fe92d94ef8f
Manoj <manojgharti691@gmail.com>
2026-02-06 07:25:01 +0545

    code

---
+++ String/Learning string/string.py
@@ -0,0 +1,11 @@
+name = "Manoj"      #way to write string and string is immutable
+address = 'Rolpa'
+school = '''Jana sewa'''
+
+#slice the string
+ 
+nameShort = name[0:4] # 0 to 3 not include 4 index string 
+nameShort2 = name[-5:-1] # -5 to -1 not include -1 index string where -5 equvalent to 0and -1 to 4
+
+print(nameShort)
+print(nameShort2)
