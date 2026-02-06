e09e836f8ca9b704c4b50ffeefcbf183fb20a4eb
Manoj <manojgharti691@gmail.com>
2026-02-06 07:32:15 +0545

    code

--- String/Learning string/string.py
+++ String/Learning string/string.py
@@ -9,3 +9,10 @@ nameShort2 = name[-5:-1] # -5 to -1 not include -1 index string where -5 equvale
 
 print(nameShort)
 print(nameShort2)
+
+#Slicing with skip value
+word = "Kathmandu"
+word1 = word[0:9:4] # skip 3 character n-1
+print(word1)
+print(word[:9]) #[:9] = [0:9]
+print(word[0:]) # [0:]= [0:9]
