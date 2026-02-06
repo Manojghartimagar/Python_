d2f084c5ce2e0ba6f17bef858a10a63d24a3c6d3
Manoj <manojgharti691@gmail.com>
2026-02-06 10:41:44 +0545

    question1

---
+++ Dictionary and Sets/Practice question/Question1.py
@@ -0,0 +1,11 @@
+# Write a program to create a dictionary of Nepali words with values as their English translation provide user with an option to look it up!
+
+dic = {
+    "kera":"Banana",
+    "nayak":"Hero",
+    "Khalnayak":"Vilean",
+    "Chalchitra":"movie",
+    "Jaat":"Cast"
+}
+
+print(dic)
