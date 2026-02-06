09d75df29f7e47491bd67f330467fa90a39cb9cc
Manoj <manojgharti691@gmail.com>
2026-02-06 10:50:39 +0545

    question3

---
+++ Dictionary and Sets/Practice question/Question3.py
@@ -0,0 +1,5 @@
+# Can we have a set with 18(int) and "18"*str as a values in it?
+# yes it is possible because set donot store dublicate data but in this case one 18 is int and another 18 is string so no dublication at this situation
+
+set = (18,"18")
+print(set)
