780677226ff8ef1f1500a8079c6b585992c081a3
Manoj <manojgharti691@gmail.com>
2026-02-06 09:48:19 +0545

    Question1

--- List and Tuple/Practice Question/Question1.py
+++ List and Tuple/Practice Question/Question1.py
@@ -1,6 +1,6 @@
 #Write a program to store seven fruits in a ist entered by the user.
-list = []
-for i in range(0,7):
-    fruit = input(f"Enter {i+1} Fruits name: ")
-    list.insert(i,fruit)
-print(f"Your list is\n{list}")
+fruits = []
+for i in range(7):
+    fruit = input(f"Enter fruit {i+1} name: ")
+    fruits.append(fruit)
+print(f"Your list is:\n{fruits}")
