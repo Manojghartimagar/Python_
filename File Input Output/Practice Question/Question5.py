d3b51142a729f5f434f7a5fdf983060f9f38bc76
Manoj <manojgharti691@gmail.com>
2026-02-07 09:38:27 +0545

    file

--- File Input Output/Practice Question/Question5.py
+++ File Input Output/Practice Question/Question5.py
@@ -1,9 +1,10 @@
 # Write a program to find out the line number where python is present in paragraph.
 
-# Program to find the line numbers where "python" is present
-
-with open("Python.txt", "r") as f:
-    for count, line in enumerate(f, start=1):
-        if "python" in line.lower():
-            print("Python present in Line", count)
-
+with open("Python.txt","r") as f:
+    count = 1
+    line = f.readline().lower()
+    while(line != ""):
+        if("python" in line):
+            print("Python present in Line ",count)
+        count+=1
+        line = f.readline().lower()
